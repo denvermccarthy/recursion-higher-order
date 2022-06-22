a17770c5fdda1db6d7567883f4c119cddaaa5e60
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-22 11:11:28 -0700

    addFirst working, cheeky adding more than one added for FUN

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
 // import { testEvery, testSome } from "./tests";
 
 class Arr {
-  array;
-  constructor(input) {
-    this.array = [...input];
-  }
-  forEachCus(func) {
-    for (let i = 0; i < this.array.length; i++) {
-      func(this.array[i]);
+    array;
+    constructor(input) {
+        this.array = [...input];
     }
-  }
-  mapCus(func) {
-    const newArr = [];
-    for (let i = 0; i < this.array.length; i++) {
-      const value = func(this.array[i]);
-      newArr.push(value);
+    forEachCus(func) {
+        for (let i = 0; i < this.array.length; i++) {
+            func(this.array[i]);
+        }
     }
-    return newArr;
-  }
-  filterCus(func) {
-    const newArr = [];
-    for (let i = 0; i < this.array.length; i++) {
-      const item = this.array[i];
-      const bool = func(item);
-      if (bool) {
-        newArr.push(item);
-      }
+    mapCus(func) {
+        const newArr = [];
+        for (let i = 0; i < this.array.length; i++) {
+            const value = func(this.array[i]);
+            newArr.push(value);
+        }
+        return newArr;
     }
-    return newArr;
-  }
-  everyCus(func) {
-    const newArr = [];
-    for (let i = 0; i < this.array.length; i++) {
-      const item = this.array[i];
-      const bool = func(item);
-      if (bool) {
-        newArr.push(item);
-      }
+    filterCus(func) {
+        const newArr = [];
+        for (let i = 0; i < this.array.length; i++) {
+            const item = this.array[i];
+            const bool = func(item);
+            if (bool) {
+                newArr.push(item);
+            }
+        }
+        return newArr;
     }
-    return newArr.length === this.array.length;
-  }
-  someCus(func) {
-    for (let i = 0; i < this.array.length; i++) {
-      const item = this.array[i];
-      const bool = func(item);
-      if (bool) return true;
+    everyCus(func) {
+        const newArr = [];
+        for (let i = 0; i < this.array.length; i++) {
+            const item = this.array[i];
+            const bool = func(item);
+            if (bool) {
+                newArr.push(item);
+            }
+        }
+        return newArr.length === this.array.length;
     }
-    return false;
-  }
+    someCus(func) {
+        for (let i = 0; i < this.array.length; i++) {
+            const item = this.array[i];
+            const bool = func(item);
+            if (bool) return true;
+        }
+        return false;
+    }
 }
 const array = new Arr([1, 2, 3, 4]);
 // testEvery();
 // testSome();
 
 const fib = (n) => {
-  if (n < 2) {
-    return n;
-  }
-  return fib(n - 2) + fib(n - 1);
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 2) + fib(n - 1);
 };
 console.log(fib(37));
 //attempted memoization solution, it works, but it is much much slower. Any idea why?
@@ -74,11 +74,28 @@ console.log(fib(37));
 
 const addPunc = (punc) => (sentence) => sentence + punc;
 
-const addExcitement = addPunc("!!!");
-console.log(addExcitement("Hello World"));
+const addExcitement = addPunc('!!!');
+console.log(addExcitement('Hello World'));
 // Hello World!!!
-const addUnsure = addPunc("?!?");
-console.log(addUnsure("Hello World"));
+const addUnsure = addPunc('?!?');
+console.log(addUnsure('Hello World'));
 // Hello World?!?
-console.log(addUnsure("Pokemon, catch em all"));
+console.log(addUnsure('Pokemon, catch em all'));
 // Pokemon, catch em all?!?
+
+const addFirst =
+    (...item) =>
+    (arr) => {
+        arr.unshift(...item);
+        return arr;
+    };
+const addOrange = addFirst('orange', 'apple');
+console.log(addOrange(['red', 'blue', 'green']));
+// [orange,red,blue,green]
+console.log(addOrange(['blue', 'blue', 'blue']));
+// [orange,blue,blue,blue]
+const addCat = addFirst('cat');
+console.log(addOrange(['dog', 'bird', 'lizard']));
+// [cat,dog,bird,lizard]
+console.log(addOrange(['lizard', 'donkey', 'whale']));
+// [cat,lizard,donkey,whale]
