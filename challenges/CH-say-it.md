32436e874335fb7b9b7e98a9d3c84f4094a3391f
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-22 15:09:15 -0700

    added sayIt

--- challenges/CH-say-it.md
+++ challenges/CH-say-it.md
@@ -1,13 +1,12 @@
-Implement `sayIt`
----
+## Implement `sayIt`
 
 ## Challenge
 
-Write a chainable function that accepts on word per function call, but when called without arguments, 
+Write a chainable function that accepts one word per function call, but when called without arguments,
 will report back all the previously passed words in order.
 
 ```js
-const result = sayIt('hello')('my')('name')('is')('JavaScript')();
+const result = sayIt("hello")("my")("name")("is")("JavaScript")();
 
 console.log(result);
 // "hello my name is JavaScript"
@@ -16,11 +15,11 @@ console.log(result);
 Each call returns a function that can be called, same as:
 
 ```js
-const result1 = sayIt('hello')
-const result2 = result1('my');
-const result3 = result2('name');
-const result4 = result3('is')
-const result5 = result4('JavaScript')
+const result1 = sayIt("hello");
+const result2 = result1("my");
+const result3 = result2("name");
+const result4 = result3("is");
+const result5 = result4("JavaScript");
 const result = result5();
 
 console.log(result);
--- index.js
+++ index.js
@@ -1,63 +1,63 @@
 // import { testEvery, testSome } from "./tests";
 
 class Arr {
-    array;
-    constructor(input) {
-        this.array = [...input];
+  array;
+  constructor(input) {
+    this.array = [...input];
+  }
+  forEachCus(func) {
+    for (let i = 0; i < this.array.length; i++) {
+      func(this.array[i]);
     }
-    forEachCus(func) {
-        for (let i = 0; i < this.array.length; i++) {
-            func(this.array[i]);
-        }
+  }
+  mapCus(func) {
+    const newArr = [];
+    for (let i = 0; i < this.array.length; i++) {
+      const value = func(this.array[i]);
+      newArr.push(value);
     }
-    mapCus(func) {
-        const newArr = [];
-        for (let i = 0; i < this.array.length; i++) {
-            const value = func(this.array[i]);
-            newArr.push(value);
-        }
-        return newArr;
+    return newArr;
+  }
+  filterCus(func) {
+    const newArr = [];
+    for (let i = 0; i < this.array.length; i++) {
+      const item = this.array[i];
+      const bool = func(item);
+      if (bool) {
+        newArr.push(item);
+      }
     }
-    filterCus(func) {
-        const newArr = [];
-        for (let i = 0; i < this.array.length; i++) {
-            const item = this.array[i];
-            const bool = func(item);
-            if (bool) {
-                newArr.push(item);
-            }
-        }
-        return newArr;
+    return newArr;
+  }
+  everyCus(func) {
+    const newArr = [];
+    for (let i = 0; i < this.array.length; i++) {
+      const item = this.array[i];
+      const bool = func(item);
+      if (bool) {
+        newArr.push(item);
+      }
     }
-    everyCus(func) {
-        const newArr = [];
-        for (let i = 0; i < this.array.length; i++) {
-            const item = this.array[i];
-            const bool = func(item);
-            if (bool) {
-                newArr.push(item);
-            }
-        }
-        return newArr.length === this.array.length;
+    return newArr.length === this.array.length;
+  }
+  someCus(func) {
+    for (let i = 0; i < this.array.length; i++) {
+      const item = this.array[i];
+      const bool = func(item);
+      if (bool) return true;
     }
-    someCus(func) {
-        for (let i = 0; i < this.array.length; i++) {
-            const item = this.array[i];
-            const bool = func(item);
-            if (bool) return true;
-        }
-        return false;
-    }
+    return false;
+  }
 }
 const array = new Arr([1, 2, 3, 4]);
 // testEvery();
 // testSome();
 
 const fib = (n) => {
-    if (n < 2) {
-        return n;
-    }
-    return fib(n - 2) + fib(n - 1);
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 2) + fib(n - 1);
 };
 console.log(fib(37));
 //attempted memoization solution, it works, but it is much much slower. Any idea why?
@@ -68,50 +68,50 @@ console.log(fib(37));
 // if (cache[n]) {
 //   return cache[n];
 // }
-
 // cache[n] = fib(n - 2) + fib(n - 1);
 // return cache[n];
 
 const addPunc = (punc) => (sentence) => sentence + punc;
 
-const addExcitement = addPunc('!!!');
-console.log(addExcitement('Hello World'));
+const addExcitement = addPunc("!!!");
+console.log(addExcitement("Hello World"));
 // Hello World!!!
-const addUnsure = addPunc('?!?');
-console.log(addUnsure('Hello World'));
+const addUnsure = addPunc("?!?");
+console.log(addUnsure("Hello World"));
 // Hello World?!?
-console.log(addUnsure('Pokemon, catch em all'));
+console.log(addUnsure("Pokemon, catch em all"));
 // Pokemon, catch em all?!?
 
+// this function will add the first item still, but I've modified it to also take as many arguements as you pass it
 const addFirst =
-    (...item) =>
-    (arr) => {
-        arr.unshift(...item);
-        return arr;
-    };
-const addOrange = addFirst('orange', 'apple');
-console.log(addOrange(['red', 'blue', 'green']));
+  (...item) =>
+  (arr) => {
+    arr.unshift(...item);
+    return arr;
+  };
+const addOrange = addFirst("orange", "apple");
+console.log(addOrange(["red", "blue", "green"]));
 // [orange,red,blue,green]
-console.log(addOrange(['blue', 'blue', 'blue']));
+console.log(addOrange(["blue", "blue", "blue"]));
 // [orange,blue,blue,blue]
-const addCat = addFirst('cat');
-console.log(addOrange(['dog', 'bird', 'lizard']));
+const addCat = addFirst("cat");
+console.log(addOrange(["dog", "bird", "lizard"]));
 // [cat,dog,bird,lizard]
-console.log(addOrange(['lizard', 'donkey', 'whale']));
+console.log(addOrange(["lizard", "donkey", "whale"]));
 // [cat,lizard,donkey,whale]
 
 function largestEven(nums) {
-    let largestNum = null;
-    //loop over each item in the array, check if even, check if number is greater than current largest
-    nums.forEach((num) => {
-        if (num % 2 == 0) {
-            if (!largestNum || largestNum <= num) {
-                largestNum = num;
-            }
-        }
-    });
-    // => largest number in an array
-    return typeof largestNum === 'number' ? largestNum : -1;
+  let largestNum = null;
+  //loop over each item in the array, check if even, check if number is greater than current largest
+  nums.forEach((num) => {
+    if (num % 2 == 0) {
+      if (!largestNum || largestNum <= num) {
+        largestNum = num;
+      }
+    }
+  });
+  // => largest number in an array
+  return typeof largestNum === "number" ? largestNum : -1;
 }
 
 console.log(largestEven([3, 7, 2, 1, 7, 9, 10, 13]));
@@ -124,22 +124,43 @@ console.log(largestEven([0, 19, 18973623]));
 //➞ 0
 
 function sum(n) {
-    let sum = 0;
-    function recursion(num) {
-        if (num > 0) {
-            sum += num;
-            return recursion(num - 1);
-        }
-        return sum;
+  let sum = 0;
+  function recursion(num) {
+    if (num > 0) {
+      sum += num;
+      return recursion(num - 1);
     }
-    return recursion(n);
+    return sum;
+  }
+  return recursion(n);
 }
-console.log('sum(5)', sum(5));
+console.log("sum(5)", sum(5));
 
 function repeat(txt, n) {
-    if (n < 1) {
-        return txt;
-    }
-    return repeat(txt, n - 1) + txt;
+  if (n < 1) {
+    return txt;
+  }
+  return repeat(txt, n - 1) + txt;
 }
-console.log('repeat', repeat('nice ', 4));
+console.log("repeat", repeat("nice ", 4));
+
+function sayIt(string) {
+  let str = "";
+  const recurse = function (arg) {
+    if (!arg) {
+      return str;
+    }
+    str += arg + " ";
+    return recurse;
+  };
+  return recurse(string);
+}
+
+const result1 = sayIt("hello");
+const result2 = result1("my");
+const result3 = result2("name");
+const result4 = result3("is");
+const result5 = result4("JavaScript");
+const result = result5();
+
+console.log(result);
