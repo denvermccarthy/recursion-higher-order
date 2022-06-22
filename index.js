62bea2232d1d5ab60eee58e0f7b33e9c8a6055ea
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-22 09:48:10 -0700

    every working

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 class Arr {
-  array;
-  constructor(input) {
-    this.array = [...input];
-  }
-  forEachCus(func) {
-    for (let i = 0; i < this.array.length; i++) {
-      func(this.array[i]);
+    array;
+    constructor(input) {
+        this.array = [...input];
     }
-  }
-  mapCus(func) {
-    const newArr = [];
-    for (let i = 0; i < this.array.length; i++) {
-      const value = func(this.array[i]);
-      newArr.push(value);
+    forEachCus(func) {
+        for (let i = 0; i < this.array.length; i++) {
+            func(this.array[i]);
+        }
     }
-    return newArr;
-  }
-  filterCus(func) {
-    const newArr = [];
-    for (let i = 0; i < this.array.length; i++) {
-      const item = this.array[i];
-      const bool = func(item);
-      if (bool) {
-        newArr.push(item);
-      }
+    mapCus(func) {
+        const newArr = [];
+        for (let i = 0; i < this.array.length; i++) {
+            const value = func(this.array[i]);
+            newArr.push(value);
+        }
+        return newArr;
     }
-    return newArr;
-  }
+    filterCus(func) {
+        const newArr = [];
+        for (let i = 0; i < this.array.length; i++) {
+            const item = this.array[i];
+            const bool = func(item);
+            if (bool) {
+                newArr.push(item);
+            }
+        }
+        return newArr;
+    }
+    everyCus(func) {
+        const newArr = [];
+        for (let i = 0; i < this.array.length; i++) {
+            const item = this.array[i];
+            const bool = func(item);
+            if (bool) {
+                newArr.push(item);
+            }
+        }
+        return newArr.length === this.array.length;
+    }
 }
 const array = new Arr([1, 2, 3, 4]);
+console.log(array.everyCus((x) => typeof x === 'number'));
+console.log(array.everyCus((x) => typeof x !== 'number'));
+console.log(array.everyCus((x) => x > 2));
